e9a782aedfce5c42953c4725de8e4bd36c003a3d
Corey <Corey255A1@hotmail.com>
2024-03-04 06:45:37 -0500

    Completely Generic Value Type

---
+++ SudokuSolver/SudokuValueInt.cpp
@@ -0,0 +1,34 @@
+#include "SudokuValueInt.h"
+
+const SudokuValueInt& SudokuValueInt::castTo(const SudokuValue& value) {
+    const auto* sudokuValueInt = dynamic_cast<const SudokuValueInt*>(&value);
+    if (sudokuValueInt == nullptr) { throw std::exception("Invalid Sudoku Value Cast: To Int"); }
+    return *sudokuValueInt;
+}
+
+const SudokuValueInt* SudokuValueInt::castTo(const SudokuValue* value) {
+    const auto* sudokuValueInt = dynamic_cast<const SudokuValueInt*>(value);
+    if (sudokuValueInt == nullptr) { throw std::exception("Invalid Sudoku Value Cast: To Int"); }
+    return sudokuValueInt;
+}
+
+bool SudokuValueInt::equals(const SudokuValue* right) const {
+    return m_value == castTo(right)->m_value;
+}
+
+bool SudokuValueInt::lessThan(const SudokuValue* right) const {
+    return m_value < castTo(right)->m_value;
+}
+
+bool SudokuValueInt::lessThanOrEqual(const SudokuValue* right) const {
+    return m_value <= castTo(right)->m_value;
+}
+
+std::unique_ptr<SudokuValue> SudokuValueInt::makeCopy() const {
+    return std::make_unique<SudokuValueInt>(m_values, m_value);
+}
+
+std::ostream& SudokuValueInt::outToStream(std::ostream& stream) const {
+    stream << m_value;
+    return stream;
+}
---
+++ SudokuSolver/SudokuValueInt.h
@@ -0,0 +1,24 @@
+#ifndef HSudokuValueInt
+#define HSudokuValueInt
+#include "SudokuValue.h"
+class SudokuValueInt : public SudokuValue
+{
+private:
+    int m_value;
+
+public:
+    SudokuValueInt(std::shared_ptr<const SudokuValueRange> values, int value) :SudokuValue(values), m_value(value) {}
+    static const SudokuValueInt& castTo(const SudokuValue& value);
+    static const SudokuValueInt* castTo(const SudokuValue* value);
+    int getValue() const { return m_value; }
+    virtual size_t getHash() const { return getValue(); }
+    bool isDefault() const { return m_values->isDefault(this); }
+    virtual bool equals(const SudokuValue* right) const;
+    virtual bool lessThan(const SudokuValue* right) const;
+    virtual bool lessThanOrEqual(const SudokuValue* right) const;
+    virtual std::ostream& outToStream(std::ostream& stream) const;
+    virtual std::unique_ptr<SudokuValue> makeCopy() const;
+    std::shared_ptr<const SudokuValueRange> getValueDefinition() const { return m_values; }
+
+};
+#endif
