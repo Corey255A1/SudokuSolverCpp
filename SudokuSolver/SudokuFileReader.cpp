68044eb5926b1ddca7bb5d8b888680f8a74c3ac1
corey255a1 <corey255a1@gmail.com>
2024-03-08 10:51:20 +0000

    Dynamically choosing file value parser

--- SudokuSolver/SudokuFileReader.cpp
+++ SudokuSolver/SudokuFileReader.cpp
@@ -23,27 +23,48 @@ std::unique_ptr<SudokuBoard> SudokuFileReader::read(const std::string& filePath)
 	//std::wstring utfContents = converter.from_bytes(fileContents.str());
 	
 	std::wstring line;
-	if (!std::getline(sudokuFile, line)) { throw std::runtime_error("Not a valid Sudoku File"); }
-	int sudokuSize = (line.length() + 1) / 2; // account for spaces
-	std::wcout << "board size:" << sudokuSize << std::endl << line << std::endl;
-	auto intValues = std::make_shared<SudokuValueIntRange>(-1, 1, sudokuSize);
+	std::shared_ptr<SudokuValueRange> valueTypeRange;
+	int sudokuSize = 0;
 
-	std::vector<std::wstring> emojis = {
-		L"🤖", L"😀", L"😃", L"😄", L"😁", L"😆", L"😛", L"😝", L"😜", L"🤪"
-	};
-	auto emojiValues = std::make_shared<SudokuValueEmojiRange>(emojis);
-
-	//std::unique_ptr<SudokuBoard> board = std::make_unique<SudokuBoard>(emojiValues);
-	std::unique_ptr<SudokuBoard> board = std::make_unique<SudokuBoard>(intValues);
+	wchar_t unicodeChar;
+	sudokuFile >> unicodeChar;
+	bool unicodeMode = false;
+	if(unicodeChar == L'U'){
+		std::vector<std::wstring> emojis;
+		// L"🤖", L"😀", L"😃", L"😄", L"😁", L"😆", L"😛", L"😝", L"😜", L"🤪"
+		
+		std::wstring characterLine;
+		std::getline(sudokuFile, characterLine);
+		std::wstringstream characterStream(characterLine);
+		while(!characterStream.eof()){
+			std::wstring character;
+			characterStream >> character;
+			emojis.push_back(character);
+		}
+		valueTypeRange = std::make_shared<SudokuValueEmojiRange>(emojis);
+		sudokuSize = emojis.size() - 1;
+		if (!std::getline(sudokuFile, line)) { throw std::runtime_error("Not a valid Sudoku File"); }
+	} 
+	else
+	{
+		sudokuFile.seekg(0);
+		if (!std::getline(sudokuFile, line)) { throw std::runtime_error("Not a valid Sudoku File"); }
+		sudokuSize = (line.length() + 1) / 2; // account for spaces
+		valueTypeRange = std::make_shared<SudokuValueIntRange>(-1, 1, sudokuSize);
+	}
 	
+	std::wcout << "Board Size: " << sudokuSize << std::endl << line << std::endl;
+	std::unique_ptr<SudokuBoard> board = std::make_unique<SudokuBoard>(valueTypeRange);
+
 	int row = 0;
-	processLine(*intValues, line, row, board.get());
+	processLine(*valueTypeRange, line, row, board.get());
 	while (std::getline(sudokuFile, line) && row < sudokuSize-1)
 	{
 		std::wcout << line << std::endl;
 		row += 1;
-		processLine(*intValues, line, row, board.get());
+		processLine(*valueTypeRange, line, row, board.get());
 	}
+	std::wcout << "Done Reading" << std::endl;
 	return board;
 }
 
